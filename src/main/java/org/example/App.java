38c26411a9a5f2fb4eae5b84f38520ba07caf7cc
OrlayMolina <111409267+OrlayMolina@users.noreply.github.com>
2023-02-28 22:36:56 -0500

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,33 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        int[][] matriz = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
+        int sumaDiagonal = sumarDiagonal(matriz, 0, 0);
+        System.out.println(imprimirMatriz(matriz));
+        System.out.println("\nLa suma de la diagonal de la matriz: " + sumaDiagonal);
+    }
+
+    public static int sumarDiagonal(int[][] matriz, int fila, int columna) {
+        if (fila == matriz.length) {
+            return 0;
+        }
+        return matriz[fila][columna] + sumarDiagonal(matriz, fila + 1, columna + 1);
+    }
+
+    public static int [][] imprimirMatriz (int [][] matriz) {
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.print(matriz[i][j] + "\t");
+            }
+            System.out.println();
+        }
+        return matriz;
+    }
+}
